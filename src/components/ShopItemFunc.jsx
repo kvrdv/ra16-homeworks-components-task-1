ebbd2eb459f2eadf917f85fdacf94342d616a53c
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-11-19 22:50:17 +0300

    first commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Item } from '../models/Item';
+
+export function ShopItemFunc({ item }) {
+  return (
+    <>
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">
+            {item.price.toFixed(2)}
+            {item.currency}
+          </div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.instanceOf(Item),
+};
